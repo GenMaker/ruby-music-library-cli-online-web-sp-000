63205b411300bd5b1011b81de2f2e760aa6f754d
Panela Leung <leungpanela@gmail.com>
2019-11-26 04:06:27 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -47,7 +47,6 @@ class MusicLibraryController
 
   def list_artists
     Artist.all.sort{|a, b| a.name <=> b.name}.each_with_index do |a,i|
-      #binding.pry
       puts "#{i+1}. #{a.name}"
     end
   end
