2da070459b1ba13c8a4a7467cbd669dfa9b756a4
Panela Leung <leungpanela@gmail.com>
2019-11-26 03:46:45 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class MusicLibraryController
   attr_accessor :path
 
@@ -7,20 +9,52 @@ class MusicLibraryController
   end
 
   def call
-     input = gets.strip
-    # if input != exit
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
-    #end
+     input = ""
+    while input != "exit"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      input = gets.strip
 
-     input = gets.strip
+      case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_artist
+      when "list genre"
+        list_genre
+      when "play song"
+        list_play_song
+      end
+    end
   end
+
+  def list_songs
+    Song.all.sort{|a,b| a.name <=> b.name}.each_with_index do |s,i|
+      puts "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+    end
+  end
+
+  def list_artists
+    binding.pry
+    Artist.all.sort {|a,b| a.name <=> b.name}.each_with_index do |a,i|
+      puts "#{i+1}. #{a.name}"
+    end
+  end
+
+
+
+
+
 
 end
