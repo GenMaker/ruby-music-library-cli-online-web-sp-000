6cbe0c1853959884853d30487f391a3fb38449bf
Panela Leung <leungpanela@gmail.com>
2019-11-26 04:38:30 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -82,7 +82,7 @@ class MusicLibraryController
   end
 
   def play_song
-    list_songs
+    
     puts "Which song number would you like to play?"
     input = gets.strip.to_i
     if input > 0 && input<= Song.all.length
