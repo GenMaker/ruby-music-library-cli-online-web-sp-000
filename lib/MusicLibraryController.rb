5fc5b58e169e624500aa4808f96818f116bf3d92
Panela Leung <leungpanela@gmail.com>
2019-11-26 04:43:45 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -30,9 +30,9 @@ class MusicLibraryController
       when "list genres"
         list_genres
       when "list artist"
-        list_artist
+        list_songs_by_artist
       when "list genre"
-        list_genre
+        list_songs_by_genre
       when "play song"
         list_play_song
       end
